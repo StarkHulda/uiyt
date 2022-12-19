91294c97c3c5223d9d5197a53cd53b5d1a70d17b
StarkHulda <112618068+StarkHulda@users.noreply.github.com>
2022-12-19 08:54:42 +0000

    Update start.sh

--- Render-CleverCloud-xray/start.sh
+++ Render-CleverCloud-xray/start.sh
@@ -4,7 +4,7 @@
 export UUID=7562dc4f-02bb-4349-b970-0d3e5a8019ec
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=mirror.umd.edu
+export ProxySite=netsrepublic.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
